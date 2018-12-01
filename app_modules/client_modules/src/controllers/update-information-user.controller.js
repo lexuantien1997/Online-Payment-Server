33ea3486d1e7d78d4a78aeb3315522ad733a1883
Huynh Minh Tan <minhtan.itdev@gmail.com>
2018-12-01 15:44:09 +0700

    Check email when change informarion user

--- app_modules/client_modules/src/controllers/update-information-user.controller.js
+++ app_modules/client_modules/src/controllers/update-information-user.controller.js
@@ -1,6 +1,6 @@
 const User = require('../../../../database/app/user');
-const forgotPassService = require('../services/forgotPassword.service');
 const errorsName = require('../validations/errors-name');
+const loginService = require('../services/login.service');
 
 const api = {
     status: 1, // 1: fail _ 0: success
@@ -16,57 +16,73 @@ module.exports = (req,res) => {
     let user = req.body;
     console.log(user);
 
-    User.findOneAndUpdate(
-        {phone: user.phone},
-        {
-                name: user.name,
-                email: user.email,
-                address: user.address,
-                birthday: user.birthday,
-                gender: user.gender
-        },
-        {
-            new: true
-        },
-        (error, usercollection) => {
-
-            if (error) {
-                api.user = {};
+    // Check email
+    loginService.checkEmail(user.email).then (userResult => {
+        
+        if (userResult) {
+            if (user.phone != userResult.phone) {
                 api.status = 1;
-                api.errors.databse = errorsName.UPDATE_INFOR_USER_ERROR;
-                return res.status(200).json(api);   
-            }
-
-            if (usercollection == null){
-                api.user = {};
-                api.status = 1;
-                api.errors.phone = errorsName.UPDATE_INFOR_USER_PHONE_NOT_EXIST;
+                api.errors.email = errorsName.EMAIL_EXIST;
+                api.user = {};     
                 return res.status(200).json(api);
             }
-
-            let { 
-                _id, name, phone, money, gender, memberAt,
-                 email, birthday, isFirstTime, address,avatar
-            } = usercollection;
-
-            api.user = {
-                id: _id,
-                name: name,
-                phone: phone,
-                money: money,
-                gender: gender,
-                memberAt: memberAt,
-                address: address,
-                email: email,
-                birthday: birthday,
-                isFirstTime: isFirstTime,
-                avatar:avatar
-            };    
-
-            api.errors = {};
-            api.status = 0;
-            return res.status(200).json(api);  
         }
-    )
+
+        // Update user
+        User.findOneAndUpdate(
+            {phone: user.phone},
+            {
+                    name: user.name,
+                    email: user.email,
+                    address: user.address,
+                    birthday: user.birthday,
+                    gender: user.gender
+            },
+            {
+                new: true
+            },
+            (error, usercollection) => {
+
+                if (error) {
+                    api.user = {};
+                    api.status = 1;
+                    api.errors.databse = errorsName.UPDATE_INFOR_USER_ERROR;
+                    return res.status(200).json(api);   
+                }
+
+                if (usercollection == null){
+                    api.user = {};
+                    api.status = 1;
+                    api.errors.phone = errorsName.UPDATE_INFOR_USER_PHONE_NOT_EXIST;
+                    return res.status(200).json(api);
+                }
+
+                let { 
+                    _id, name, phone, money, gender, memberAt,
+                    email, birthday, isFirstTime, address,avatar
+                } = usercollection;
+
+                api.user = {
+                    id: _id,
+                    name: name,
+                    phone: phone,
+                    money: money,
+                    gender: gender,
+                    memberAt: memberAt,
+                    address: address,
+                    email: email,
+                    birthday: birthday,
+                    isFirstTime: isFirstTime,
+                    avatar:avatar
+                };    
+
+                api.errors = {};
+                api.status = 0;
+                return res.status(200).json(api);  
+            }
+        )
+    });  
+
+    
 
 }
